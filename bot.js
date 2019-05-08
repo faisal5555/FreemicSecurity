65500cd4017d4b35895ad4286beaeae7367efaad
faisal5555 <44726870+faisal5555@users.noreply.github.com>
2019-05-08 21:11:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`FreeMic Security.`,"http://twitch.tv//idk")
+client.user.setGame(`BadStation Security`,"http://twitch.tv//idk")
 });
 
 
